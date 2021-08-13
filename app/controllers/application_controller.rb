72cb6f7d4a0bc8f0db2cf00e3325f624e53729af
Paru871 <mahalo0521@gmail.com>
2021-08-13 12:24:53 +0900

    rubocop ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  
-  # deviseコントローラーにストロングパラメータを追加する          
+
+  # deviseコントローラーにストロングパラメータを追加する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
     # アカウント編集の時にzipcode,address,profileのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:account_update, keys: [:zipcode, :address, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[zipcode address profile])
   end
-
 end
