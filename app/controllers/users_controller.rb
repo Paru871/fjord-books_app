56e2110baf4bcd57150ea07533436d045f43d7b9
Paru871 <mahalo0521@gmail.com>
2021-08-14 14:08:38 +0900

    バリデーション以外は完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,29 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  # def create
+  #   user = User.create!(user_params)
+  #   redirect_to root_path
+  # end
+
+  # def update
+  #   if @user.update(user_params) && @user.avatar.attach(params[:avatar])
+  #     redirect_to user_path(@user)
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  private
+    
+  def user_params
+      params.require(:user).permit(:name, :postal_code, :address, :self_introduction, :avatar)
   end
 end
