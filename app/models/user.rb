61254438110284924255ea89a166d36dc9f32388
Paru871 <mahalo0521@gmail.com>
2021-08-15 16:19:47 +0900

    アップロードファイル形式のバリデーションを実装しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
-  # validate :image_type
+  validate :image_type
 
-  def thumbnail
-    avatar.variant(resize_to_fit: [100, 100]).processed
+  private
+
+  def image_type
+    return unless avatar.attached?
+
+    return if avatar.blob.content_type.in?(%('image/jpeg image/jpg image/png image/gif'))
+
+    errors.add(:avatar, I18n.t('wrong_content_type'))
   end
-
-  # private
-
-  # def image_type
-  #   return unless avatar.attached?
-
-  #   return if avatar.blob.content_type.in?(%('image/jpeg image/jpg image/png image/gif'))
-
-  #   errors.add(:avatar, I18n.t('errors.messeges.wrong_content_type'))
-  # end
 end
