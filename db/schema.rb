0af641b3ffb9ce76d95d6257edeeb8e0732b6882
Paru871 <mahalo0521@gmail.com>
2021-08-18 22:54:07 +0900

    日報の日本語化を行った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_080708) do
+ActiveRecord::Schema.define(version: 2021_08_18_133849) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_080708) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
