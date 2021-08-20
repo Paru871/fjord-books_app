ecdde07c1a40d678f96df1a8df420453d848d0f9
Paru871 <mahalo0521@gmail.com>
2021-08-20 09:56:55 +0900

    バリデーション試行錯誤中

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
-  validate :image_type
+  validate :avatar_type
 
   private
 
-  def image_type
+  def avatar_type
     return unless avatar.attached?
+    puts avatar.blob.content_type
+    
+    errors.add(:avatar, I18n.t('wrong_content_type')) if avatar.blob.content_type == 'application/pdf'
+    #   puts "yes"
+    #   errors[:base] << "PDFは不可"
+    #   puts "yes2"
+    # else
+    #   puts "no"
+    # end
 
-    return if avatar.blob.content_type.in?(%('image/jpeg image/jpg image/png image/gif'))
-
+    return if avatar.blob.content_type.in?(%w('image/jpeg image/jpg image/png image/gif'))
     errors.add(:avatar, I18n.t('wrong_content_type'))
   end
+
 end
