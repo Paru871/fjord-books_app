232e6486a503768d386d694816af78dfe4fb44ec
Paru871 <mahalo0521@gmail.com>
2021-08-20 14:48:39 +0900

    バリデーションのエラー修正終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,9 @@ class User < ApplicationRecord
 
   def avatar_type
     return unless avatar.attached?
-    puts avatar.blob.content_type
-    
-    errors.add(:avatar, I18n.t('wrong_content_type')) if avatar.blob.content_type == 'application/pdf'
-    #   puts "yes"
-    #   errors[:base] << "PDFは不可"
-    #   puts "yes2"
-    # else
-    #   puts "no"
-    # end
 
-    return if avatar.blob.content_type.in?(%w('image/jpeg image/jpg image/png image/gif'))
-    errors.add(:avatar, I18n.t('wrong_content_type'))
+    return if avatar.blob.content_type.in?(%w['image/jpeg image/jpg image/png image/gif'])
+
+    errors.add(:avatar, I18n.t('devise.registrations.edit.wrong_content_type'))
   end
-
 end
