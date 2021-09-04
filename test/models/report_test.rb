8217c8f8d7c01242623e87e8e45e7b9b55e906f7
Paru871 <mahalo0521@gmail.com>
2021-09-05 02:10:57 +0900

    テスト作成1日目終了

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#created_on' do
+    # me = User.create!(email: 'me@example.com', password: 'password')
+    @user = users(:alice)
+
+    report = Report.create!(user: @user, title: 'title', content: 'content')
+
+    assert_not_equal report.created_at.class, Date
+    assert_equal report.created_on.class, Date
+  end
 end
