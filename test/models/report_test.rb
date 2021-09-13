0f228c9c9b7246412ba061dcc915facadb151c99
Paru871 <mahalo0521@gmail.com>
2021-09-13 12:32:27 +0900

    test #created_on 修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -13,10 +13,28 @@ class ReportTest < ActiveSupport::TestCase
     assert_not report2.editable?(user)
   end
 
+  # test '#created_on' 修正案1/3
+  # test '#created_on' do
+  #   report = reports(:one)
+  #   report.created_at = '2021-09-10'
+  #   assert_equal Date.new(2021, 9, 10), report.created_on
+  # end
+
+  # test '#created_on' 修正案2/3
+  #   test '#created_on' do
+  #   report = reports(:one)
+  #   travel_to Time.zone.parse('2021/09/10') do
+  #     report.created_at = Time.zone.now
+  #     assert_equal Date.today, report.created_on
+  #   end
+  # end
+
+  # test '#created_on' 修正案3/3
   test '#created_on' do
     report = reports(:one)
-
-    assert report.created_on
-    assert_not_equal report.created_at, report.created_on
+    travel_to Time.zone.parse('2021/09/10') do
+      report.created_at = Time.zone.now
+    end
+    assert_equal Date.new(2021, 9, 10), report.created_on
   end
 end
